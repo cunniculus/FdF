838592d6bcedf59430a4063f033b0d5424985d50
gui-cuniculus <cuniculus.gui@gmail.com>
2022-10-05 12:01:34 -0300

    event basics

--- main.c
+++ main.c
@@ -1,41 +1,43 @@
 #include <mlx.h>
+#include <libft.h>
+#include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
-
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
+int	deal_key(int key, void *param)
+{
+	ft_putchar('x');
+	return (0);
+}
 
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
-	t_data	img;
-	img.bits_per_pixel = 5;
-	img.line_length = 5;
 
+	// mlx_init function initializes the connection between 
+	// your software and the display.
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	if (!mlx)
+		return (-1);
+	mlx_win = mlx_new_window(mlx, 500, 500, "Hello world!");
+	mlx_pixel_put(mlx, mlx_win, 250, 250, 0xFFFFFF);
+	mlx_key_hook(mlx_win, deal_key, (void *)0);
 
-	img.img = mlx_new_image(mlx, 1920, 1080);
-
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x01FFFFFF);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	// allows events to be received: 
+	// 		key is pressed;
+	// 		mouse button is pressed
+	// 		part of the windows should be re-drawn (expose)
+	// mlx_loop never returns; when an event happens, it calls a
+	// user-defined function associated with this event.
+	// each event can be assigned to different functions.
+	// each window can define a different fucntion for the same event
+	//
 	mlx_loop(mlx);
+
 	return (0);
 }
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
