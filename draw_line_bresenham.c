4ceb57745898bcc05266bafb00d70cf394f2074e
gui-cuniculus <cuniculus.gui@gmail.com>
2022-10-10 21:08:56 -0300

    added some error checking for draw_line_brasenham, fixed memory leaks

--- draw_line_bresenham.c
+++ draw_line_bresenham.c
@@ -1,7 +1,6 @@
 #include <mlx.h>
 #include <math.h>
 #include "fdf.h"
-#include<stdio.h>
 
 #ifndef WIDTH
 # define WIDTH 900
@@ -10,6 +9,8 @@
 #ifndef HIGHT
 # define HIGHT 900
 #endif
+
+#define MLX_ERROR -1
 
 typedef struct	s_data {
 	void	*img;
@@ -38,16 +39,27 @@ int	main(void)
 	t_data	img;
 
 	mlx = mlx_init();
+	if (mlx == NULL)
+		return (MLX_ERROR);
 	mlx_win = mlx_new_window(mlx, WIDTH, HIGHT, "Hello world!");
+	if (mlx_win == NULL)
+	{
+		free(mlx_win);
+		return (MLX_ERROR);
+	}
 	img.img = mlx_new_image(mlx, WIDTH, HIGHT);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 				&img.endian);
 
-	printf("foi\n");
 	plot_line(0,0, WIDTH, HIGHT, img); 
 
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
+
+	/* we will exit the loop if there's no window left, and execute this code */
+	mlx_destroy_image(mlx, img.img);
+	mlx_destroy_display(mlx);
+	free(mlx);
 }
 
 
@@ -60,22 +72,14 @@ void plot_line(int x0, int y0, int x1, int y1, t_data img)
         if (x0 > x1)
             plot_line_low(x1, y1, x0, y0, img);
         else
-		{
             plot_line_low(x0, y0, x1, y1, img);
-		}
 	}
     else
 	{
-		printf("foi %d\n", x1);
         if (y0 > y1)
-		{
-			printf("foi %d\n", x1);
             plot_line_high(x1, y1, x0, y0, img);
-		}
         else
-		{
             plot_line_high(x0, y0, x1, y1, img);
-		}
 	}
 }
 
@@ -113,13 +117,11 @@ void plot_line_high(int x0, int y0, int x1, int y1, t_data img)
 	int	xi;
 	int	diff;
 
-	printf("high %d\n", y1);
 	dx = x1 - x0;
     dy = y1 - y0;
     xi = 1;
     if (dx < 0)
 	{
-		printf("high inside first if %d\n", y1);
         xi = -1;
         dx = -dx;
 	}
@@ -128,7 +130,6 @@ void plot_line_high(int x0, int y0, int x1, int y1, t_data img)
 	int y = y0;
 	while (y < y1)
 	{
-		printf("high inside while: x==%d y==%d\n", x, y);
 		my_mlx_pixel_put(&img, x, y, 0x00FFFFFF);
         if (diff > 0)
 		{
